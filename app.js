cc9ae4d13b00044fe8e4e15159b124f0fa4a70ea
Ege <egecam000@gmail.com>
2024-01-14 22:59:20 +0300

    update getDetails as an async function

--- app.js
+++ app.js
@@ -12,3 +12,21 @@ const bu_mu_yani = "5EIiOWjqVsr6WMvzZwjdxV";
 let showName = "";
 let showDesc = "";
 let showPublisher = "";
+
+const getDetails = async () => {
+  try {
+    let showDetails = await getPodcast(farkli_dusun);
+    let showName = showDetails[0];
+
+    showDetails = await getPodcast(meksika_acmazi);
+    let showDesc = showDetails[1];
+
+    showDetails = await getPodcast(stoikyasam);
+    let showPublisher = showDetails[2];
+
+    return { showName, showDesc, showPublisher };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
