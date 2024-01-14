5089dcca7a018c89836f01872b4cda4903a43851
Ege <egecam000@gmail.com>
2024-01-14 22:28:16 +0300

    some sampleData to test

--- spotify-api.js
+++ spotify-api.js
@@ -55,3 +55,9 @@ const getPodcast = async (podcast_id) => {
     console.log(error);
   }
 };
+
+const farkli_dusun = "3imVa0uYZnmgrqKHj3o8Sq";
+const pop = "0TezKJDp9Z977SSZLSpMNo";
+const meksika_acmazi = "1g3XuO31nyfA88ZdD8yWBP";
+const stoikyasam = "12xYY9rBh4wLSADd02g6uH";
+const bu_mu_yani = "5EIiOWjqVsr6WMvzZwjdxV";
